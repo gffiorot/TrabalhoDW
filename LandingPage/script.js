17e56ce4cd0e689fe8c97b807e711737ba84fc1b
Gustavo Firmem <gffiorot@outlook.com>
2025-12-08 19:40:12 -0300

    Commit de revisão e finalização da parte front

--- LandingPage/script.js
+++ LandingPage/script.js
@@ -1,11 +1,41 @@
-botao = document.querySelector('.botaoIniciar');
-paginaDestino = 'registro.html'; 
+// Parte do index
+botao1 = document.querySelector('.botaoIniciar');
+paginaDestino1 = 'registro.html'; 
+if (botao1) {
+    botao1.addEventListener('click', function() {
+        window.location.href = paginaDestino1;
+    });
+}
 
+// Parte do cadastro
+botao2 = document.querySelector('.botaoRegistrar');
+paginaDestino2 = 'pagamento.html'; 
+if (botao2) { 
+    // tem que colocar o if e o prevent por ser formulario
+    botao2.addEventListener('click', function(event) {
+        event.preventDefault(); 
+        window.location.href = paginaDestino2;
+    });
+}
 
-botao.addEventListener('click', function() {
-    
+// Parte do login
+botao3 = document.querySelector('.botaoLogin');
+paginaDestino3 = 'catalogo.html'; 
+if (botao3) { 
+    // tem que colocar o if e o prevent por ser formulario
+    botao3.addEventListener('click', function(event) {
+        event.preventDefault(); 
+        window.location.href = paginaDestino3;
+    });
+}
 
-    console.log('Botão clicado! Redirecionando...');
-
-    window.location.href = paginaDestino;
-});
+// Parte do pagamento
+botao4 = document.querySelector('.botaoPagar');
+paginaDestino4 = 'catalogo.html'; 
+if (botao4) { 
+    // tem que colocar o if e o prevent por ser formulario
+    botao4.addEventListener('click', function(event) {
+        event.preventDefault(); 
+        window.location.href = paginaDestino4;
+    });
+}
